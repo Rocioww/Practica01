e4c1b157a392342065ca163f6746015d427eddba
Rocioww <rociovillalonvalle@gmail.com>
2023-11-11 13:22:59 +0100

    Arreglos y primer botón href

--- js/script.js
+++ js/script.js
@@ -47,3 +47,12 @@ function changeImage(element) {
     var originalImageUrl = element.getAttribute('data-original-image');
     originalImage.src = originalImageUrl;
   }
+
+//   window.addEventListener('scroll', function() {
+//     var header = document.querySelector('.header');
+//     if (window.scrollY > 0) {
+//       header.classList.add('header-scroll');
+//     } else {
+//       header.classList.remove('header-scroll');
+//     }
+//   });
