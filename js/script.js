778af25c8f66b9c792709388cd0817942ebc4bdd
Rocioww <rociovillalonvalle@gmail.com>
2023-11-11 13:59:46 +0100

    Botones

--- js/script.js
+++ js/script.js
@@ -48,11 +48,3 @@ function changeImage(element) {
     originalImage.src = originalImageUrl;
   }
 
-//   window.addEventListener('scroll', function() {
-//     var header = document.querySelector('.header');
-//     if (window.scrollY > 0) {
-//       header.classList.add('header-scroll');
-//     } else {
-//       header.classList.remove('header-scroll');
-//     }
-//   });
